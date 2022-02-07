954bdf4cc0fdedb5708bdc4404c05c14c6c0cba6
Zachary Sirna <zsirna@yahoo.com>
2022-02-07 11:29:02 -0500

    finished adding images, created first listener

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const clickBoulder = document.getElementById("boulder");
+const yellow1 = document.getElementById("yellow-1");
 
-clickBoulder.addEventListener ('click', function() {
-    alert ("i was clicked");
-});
+function highlight() {
+  yellow1.style.display = "block";
+}
+
+const boulder = document.getElementById("boulder");
+
+boulder.addEventListener('click', highlight);
