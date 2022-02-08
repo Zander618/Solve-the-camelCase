9c53c4159d0c18119049d209a86aa1c4d2a9a0d4
Zachary Sirna <zsirna@yahoo.com>
2022-02-07 19:36:51 -0500

    added event listeners to highlight when clicked, added button for fun, need if_else statements to create puzzle

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const yellow1 = document.getElementById("yellow-1");
 
+//function
+const psyduck = document.getElementById("psyduck")
+
+psyduck.addEventListener("click", function (){
+  alert ("Psyduck is no longer your friend :(")
+})
+
+//function 1
 function highlight() {
   yellow1.style.display = "block";
 }
@@ -7,3 +15,82 @@ function highlight() {
 const boulder = document.getElementById("boulder");
 
 boulder.addEventListener('click', highlight);
+
+//function 2
+const yellow2 = document.getElementById("yellow-2");
+
+function highlight2() {
+  yellow2.style.display = "block";
+}
+
+const cascade = document.getElementById("cascade");
+
+cascade.addEventListener('click', highlight2);
+
+//function3
+const yellow3 = document.getElementById("yellow-3");
+
+function highlight3() {
+  yellow3.style.display = "block";
+}
+
+const thunder = document.getElementById("thunder");
+
+thunder.addEventListener('click', highlight3);
+
+//function4
+const yellow4 = document.getElementById("yellow-4");
+
+function highlight4() {
+  yellow4.style.display = "block";
+}
+
+const rainbow = document.getElementById("rainbow");
+
+rainbow.addEventListener('click', highlight4);
+
+//function5
+const yellow5 = document.getElementById("yellow-5");
+
+function highlight5() {
+  yellow5.style.display = "block";
+}
+
+const soul = document.getElementById("soul");
+
+soul.addEventListener('click', highlight5);
+
+//function6
+const yellow6 = document.getElementById("yellow-6");
+
+function highlight6() {
+  yellow6.style.display = "block";
+}
+
+const marsh = document.getElementById("marsh");
+
+marsh.addEventListener('click', highlight6);
+
+//function7
+const yellow7 = document.getElementById("yellow-7");
+
+function highlight7() {
+  yellow7.style.display = "block";
+}
+
+const volcano = document.getElementById("volcano");
+
+volcano.addEventListener('click', highlight7);
+
+//function8
+const yellow8 = document.getElementById("yellow-8");
+
+function highlight8() {
+  yellow8.style.display = "block";
+}
+
+const earth = document.getElementById("earth");
+
+earth.addEventListener('click', highlight8);
+
+
